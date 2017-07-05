24d8dd88f3f8b8767326ed82e687df5e56396a7d
dilapitan <domlapitan@gmail.com>
2017-07-05 16:15:49 +0800

    Added codes/files for reading mulitple files alphabetically

---
+++ Multiple.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.*;
+
+class Multiple {
+	public static void main(String args[]) {
+		try {
+			File dir = new File("/home/dilapitan/Desktop/xml-parsing/files/");
+			File[] listOfFiles = dir.listFiles();
+			Arrays.sort(listOfFiles);
+			
+			for (File path : listOfFiles) {
+				System.out.println(path);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
